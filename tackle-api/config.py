68072317195b34ffe9b0e12fe2978d9ab3faa67e
Anish Malhotra <anishmalhotra97@gmail.com>
2023-04-30 20:03:16 -0400

    Add connexion wrapper and controller stubs

--- tackle-api/config.py
+++ tackle-api/config.py
@@ -1,9 +1,21 @@
-from flask import Flask
+import connexion
+
+from swagger_ui_bundle import swagger_ui_3_path
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-app = Flask(__name__)
-app.config.from_pyfile('db_config.py')
-CORS(app)
 
-db = SQLAlchemy(app)
+options = {"swagger_path": swagger_ui_3_path}
+
+app = connexion.App(__name__, specification_dir='./../')
+
+app.add_api('swagger.yaml',
+            arguments={'title': 'Tackle.Io API'},
+            options=options,
+            pythonic_params=True)
+flask_app = app.app
+flask_app.config.from_pyfile('db_config.py')
+CORS(flask_app)
+
+db = SQLAlchemy(flask_app)
---
+++ tackle-api/controllers/orders.py
@@ -0,0 +1,14 @@
+def create(body):
+    pass
+
+def read_all(product_id, page, size):
+    pass
+
+def get_by_id(id):
+    pass
+
+def update(id, body):
+    pass
+
+def delete(id):
+    pass
